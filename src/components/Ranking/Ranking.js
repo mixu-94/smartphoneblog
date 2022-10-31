0d0d12ce4385f203d49adc9ac5bfa9cde6109397
MiXu <michael.schindler894@gmail.com>
2022-10-31 17:15:21 +0100

    install axios, created useFetch

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import './Ranking.css'
+import useFetch from '../../constants/useFetch'
 
 const Ranking = () => {
+
+    const {data: smartphone, loading, error} = useFetch()
   return (
     <div>Ranking</div>
   )
--- src/constants/useFetch.js
+++ src/constants/useFetch.js
@@ -1,9 +1,48 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const API_URL = ""
+const API_URL = "";
+
+function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      }). finally(() => {
+        setLoading(false)
+      })
+
+    //   fetch('http://example.com/movies.json')
+    // .then((response) => response.json())
+    // .then((data) => console.log(data));
+  }, [url]);
+
+  const refetch = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        setError(err);
+      }). finally(() => {
+        setLoading(false)
+      })
+  }
+
+  return {data, loading, error, refetch}                 
+
+}
 
 
-
-fetch('http://example.com/movies.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+export default useFetch;

abe30e9257d99d66cf1f1994c6a6f57f52e62913
MiXu <michael.schindler894@gmail.com>
2022-11-04 15:32:11 +0100

    merge

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,8 @@ import './Navbar.css'
 import { v4 as uuidv4 } from 'uuid';
 
 const Navbar = () => {
+
+  const API_URL_HEADERNAV = "https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?access_token=2ez5kqa9TZOM-3B_THRj0kQkOUwTyCCf24WldszChbc&select=fields&content_type=headernav"
     
 
     const navbarliste = (list) => {
@@ -11,7 +13,10 @@ const Navbar = () => {
         })
     }
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      
+      
+      Navbar</div>
   )
 }
 

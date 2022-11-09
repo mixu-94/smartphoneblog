796babf87c95133d1074219c30898e6d2d15d9d6
Denis Rysavy <rysavydenis3@gmail.com>
2022-11-09 04:01:46 +0100

    kein turn mehr

--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -7,8 +7,6 @@ const Ranking = () => {
 
 const {data,loading,error} = useFetch("https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?access_token=2ez5kqa9TZOM-3B_THRj0kQkOUwTyCCf24WldszChbc&select=fields&content_type=article")
 
-console.log(data);
-
 
 
 if(data){ 
@@ -18,32 +16,34 @@ if(data){
     return <h1 style={{color:"red"}}>Error could not fetch Data</h1>
   } 
 
+
+
 const items    = data.items;
 const includes = data.includes;
-
 const images   = includes.Asset[0].fields.file.url;
+const x = items[0].fields.articleDescription.content[3]
 
-console.log({images});
+console.log(data)
+
 
 return (
   <div>
-      {    
-       items.map((index, item) => 
+      {items.map((index, item) => 
 
       <div id="inhalt" key={index}>
-      <h1>{items[item].fields.title}</h1>
-        <img src={includes.Asset[item].fields.file.url}></img>
-       { console.log(items[item].fields.title) }
-       { console.log(includes.Asset[0].fields.file.url) }
-       { console.log(items[item].fields.articleDescription.content[item].content[item])}
-       { console.log(items[item].fields.articleDescription.content[item].content[0].value)}
-       { console.log(items[item].fields.articleDescription.content[item].content[0].value)}
 
+        <h1>{items[item].fields.title}</h1>
+        <img src={includes.Asset[item].fields.file.url}></img>
         <h2>{items[0].fields.articleDescription.content[0].content[0].value}</h2>
         <p>{items[item].fields.articleDescription.content[1].content[0].value}</p>
         <h2>{items[0].fields.articleDescription.content[2].content[0].value}</h2>
+        <ul>
+          <li>{items[item].fields.articleDescription.content[3].content.map((item,index) =>{
+            return <li>{item[index]}</li>
+          })}</li>
+        </ul>
 
-
+          
 
       </div>
 
@@ -73,3 +73,11 @@ return (
 
 export default Ranking
 
+{/* <li>{x.content[1].content[0].content[0].value} :</li>
+<li>{x.content[2].content[0].content[0].value} :</li>
+<li>{x.content[3].content[0].content[0].value} :</li>
+<li>{x.content[4].content[0].content[0].value} :</li>
+<li>{x.content[5].content[0].content[0].value} :</li>
+<li>{x.content[6].content[0].content[0].value} :</li>
+<li>{x.content[7].content[0].content[0].value} :</li>
+<li>{x.content[8].content[0].content[0].value} :</li> */}

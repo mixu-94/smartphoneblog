6163439923cf8d807f442b1587e9f2808bfcf7c9
Denis Rysavy <rysavydenis3@gmail.com>
2022-11-09 01:31:28 +0100

    design und datenstruktur besser

--- src/components/Ranking/BlogInhalt.js
+++ src/components/Ranking/BlogInhalt.js
@@ -1,17 +1,14 @@
 import React from 'react'
 import "./BlogInhalt.css"
 import useFetch from '../../constants/useFetch'
-
+  
 const BlogInhalt = () => {
   return (
     <div>
-        <div id="blogInhalt">
-
-
-
+        <div id="bloginhalt">
         </div>
     </div>
-  )
+)
 }
 
 export default BlogInhalt
--- src/components/Ranking/Ranking.js
+++ src/components/Ranking/Ranking.js
@@ -4,113 +4,72 @@ import useFetch from '../../constants/useFetch'
 import { convertLength } from '@mui/material/styles/cssUtils'
 
 const Ranking = () => {
-  const URL = "https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?access_token=2ez5kqa9TZOM-3B_THRj0kQkOUwTyCCf24WldszChbc&select=fields.title,fields.articlePicture,fields.articleDescription,fields.pricePerformance,fields.conclusion,fields.linkToWebsite&content_type=article"
 
-    //const {data: smartphone, loading, error} = useFetch(url)
-    
-    const { data, loading, error } = useFetch(URL)
+const {data,loading,error} = useFetch("https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?access_token=2ez5kqa9TZOM-3B_THRj0kQkOUwTyCCf24WldszChbc&select=fields&content_type=article")
 
-
-    console.log(data);
+console.log(data);
 
 
 
 if(data){ 
   if(loading){
     return <h1>Loading ...</h1>
-  }
-  if(error){
-    <h1 style={{color:"red"}}>Error could not fetch Data</h1>
-  }
-  const fields = data.items ;
-  const fields2 = data.includes;
+  }if(error){
+    return <h1 style={{color:"red"}}>Error could not fetch Data</h1>
+  } 
 
-  const fazit = fields[0].fields.conclusion.content[0].content[0].value;
-  const articleDescription = data.items[0].fields.articleDescription.content[0].content[0].value;
-  const fazitFull = fields[0].fields.conclusion.content[1].content[0].value
+const items    = data.items;
+const includes = data.includes;
 
-  const display = fields[1].fields.articleDescription.content[2].content[0].value
-  const camera = fields[0].fields.articleDescription.content[4].content[0].value
+const images   = includes.Asset[0].fields.file.url;
 
-  const hardware = fields[0].fields.articleDescription.content[6].content[0].value
-  const software = fields[0].fields.articleDescription.content[8].content[0].value
-  const rating = fields[0].fields.pricePerformance
+console.log({images});
+
+return (
+  <div>
+      {    
+       items.map((index, item) => 
+
+      <div id="inhalt" key={index}>
+      <h1>{items[item].fields.title}</h1>
+        <img src={includes.Asset[item].fields.file.url}></img>
+       { console.log(items[item].fields.title) }
+       { console.log(includes.Asset[0].fields.file.url) }
+       { console.log(items[item].fields.articleDescription.content[item].content[item])}
+       { console.log(items[item].fields.articleDescription.content[item].content[0].value)}
+       { console.log(items[item].fields.articleDescription.content[item].content[0].value)}
+
+        <h2>{items[0].fields.articleDescription.content[0].content[0].value}</h2>
+        <p>{items[item].fields.articleDescription.content[1].content[0].value}</p>
+        <h2>{items[0].fields.articleDescription.content[2].content[0].value}</h2>
 
 
-  console.log(data.items[0].fields.articleDescription.content[0].content[0].value)
-  console.log(fields[0].fields.conclusion.content[0].content[0].value)
 
-  
-  return (
-    <div>
-        {    
-        fields.map(item => 
+      </div>
 
-        <div id="inhalt" key={item.fields.index}>
 
-          <h1  key={item.fields.title}> {item.fields.title}</h1>
+/*         <ul>
 
-          <img style={{height:"30vh"}} src={fields2.Asset[0].fields.file.url}></img>
+          <h2></h2>
+            
+          <h3></h3>
+           
+          <h2></h2>  
+           
+          <h2></h2>
+          <li></li>
+        </ul>
 
-          <h2  key={articleDescription}> {articleDescription}</h2>
-          <p   key={item.fields.articleDescription.content[1].content[0].value}> {item.fields.articleDescription.content[1].content[0].value}</p>
-
-          <ul style={{display:"flex" ,flexDirection:"column", textAlign:"center", listStyle:"none", marginLeft:"-50px"}}>
-
-            <h2 key ={display.index}>{display}</h2>
-              <li>{fields[0].fields.articleDescription.content[3].content[0].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[0].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[1].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[1].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[2].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[2].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[3].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[3].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[4].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[4].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[5].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[5].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[6].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[6].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[7].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[7].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[8].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[8].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[3].content[9].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[3].content[9].content[1].content[0].value}</li><br></br>
-
-            <h3 key ={camera}>{camera}</h3>
-              <li>{fields[0].fields.articleDescription.content[5].content[0].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[0].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[1].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[1].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[2].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[2].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[3].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[3].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[4].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[4].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[5].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[5].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[6].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[6].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[7].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[7].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[8].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[8].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[9].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[9].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[10].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[10].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[5].content[11].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[5].content[11].content[1].content[0].value}</li><br></br>
-
-            <h2>{hardware}</h2>  
-              <li>{fields[0].fields.articleDescription.content[7].content[1].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[1].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[2].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[2].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[3].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[3].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[4].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[4].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[5].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[5].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[6].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[6].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[7].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[7].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[8].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[8].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[9].content[0].content[0].value}:  <br></br>{fields[0].fields.articleDescription.content[7].content[9].content[1].content[0].value}</li><br></br>
-              <li>{fields[0].fields.articleDescription.content[7].content[10].content[0].content[0].value}: <br></br>{fields[0].fields.articleDescription.content[7].content[10].content[1].content[0].value}</li><br></br>
-
-            <h2>{software}</h2>
-              <li>{fields[0].fields.articleDescription.content[9].content[0].value}</li>
-          </ul>
-
-          <h4  key={fazit}> {fazit}</h4>
-            <p>{fazitFull}</p>
-          <h2>Rating: {item.fields.pricePerformance}/5</h2>
-          <a target="_blank" href="#"> Link zur offiziellen Webseite </a>
-        </div>
-
-       )}
-    </div>
-  )
+        <h4></h4>
+          <p></p>
+        <h2></h2>
+        <a target="_blank" href="#"></a>
+      </div> */
+     )}
+  </div>
+)
 }
 }
 
 export default Ranking
-
 

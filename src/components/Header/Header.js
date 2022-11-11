0d7b87ef4e582ca7a75c0fa80043b78921da2899
MiXu <michael.schindler894@gmail.com>
2022-11-11 17:17:41 +0100

    init navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,150 +1,22 @@
-// import React from "react";
-// import "./Header.css";
-// import useFetch from "../../constants/useFetch";
+import React from 'react'
+import './Header.css'
+import smartphonecheckvideo from '../../assets/smartphonecheck.mp4'
 
-// const Header = () => {
-  // const API_URL_HEADER =
-  //   "https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?select=fields.headertitle,fields.headerlogo&content_type=header";
-
-  // const { data, loading, error } = useFetch(API_URL_HEADER);
-
-//   let logo = null;
-//   let title = null;
-
-//   if (data) {
-//     console.log(data);
-//     const headertitle = data["items"][0]["fields"]["headertitle"];
-//     const headerurl = data["includes"]["Asset"][0]["fields"]["file"]["url"];
-//     console.log(headertitle);
-//     console.log(headerurl);
-//     // const headerimage = data.includes.Assets[0].fields.file.url
-//     // console.log(headerimage)
-
-//     // headertitle.map((item) => {
-//     //   console.log(item)
-//     //   title = item.fields.headertitle
-//     //   logo = item.fields.headerlogo.sys.id
-//     //   console.log(logo)
-
-//     return (
-//       <div className="header">
-//         <img src={headerurl} alt="test" />
-//         <div className="header-title">{headertitle}</div>
-//       </div>
-//     );
-//   }
-// };
-
-// export default Header;
-
-import React, { useState } from "react";
-import "./Header.css";
-import { FiMenu, FiX, FiTwitter } from "react-icons/fi";
-import { BsTwitter } from "react-icons/bs";
-import { FaDiscord } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
-
-const Navbar = () => {
-
-  const [menuClicked, setMenuClicked] = useState(false);
-
-  const toggleMenuClick = () => {
-    setMenuClicked(!menuClicked);
-  };
-
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-
-  const closeMenu = () => setClick(false);
-
+const Header = () => {
   return (
-    <div className="navbar__div">
-      <nav className="navbar">
-        <span className="navbar__logo">
-          <NavLink
-            to={"/"}
-            spy={true}
-            smooth={true}
-            offset={30}
-            duration={1200}
-            onClick={closeMenu}
-          >
-            
-            <img height={150} src="{}" />
-          </NavLink>
-        </span>
-        <div className="navbar__socials">
-          <a
-            className="navbar__socials_a"
-            href="https://twitter.com/"
-            title="Twitter"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <BsTwitter
-              className="navbar__socials_twitter"
-              href="https://twitter.com/"
-              title="Twitter"
-              target="_blank"
-              rel="noopener noreferrer"
-            />
-          </a>
-          <a
-            className="navbar__socials_a_discord"
-            href="http://www.discord.gg"
-            title="Discord"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaDiscord
-              className="navbar__socials_discord"
-              title="Discord"
-              target="_blank"
-              rel="noopener noreferrer"
-            />
-          </a>
-        </div>
+    <div className='header'>
+      <div className='header-rand'>
 
-        {menuClicked ? (
-          <FiX size={25} className={"navbar__menu"} onClick={toggleMenuClick} />
-        ) : (
-          <FiMenu
-            size={25}
-            className={"navbar__menu"}
-            onClick={toggleMenuClick}
-          />
-        )}
-        <ul
-          className={
-            menuClicked ? "navbar__list navbar__list--active" : "navbar__list"
-          }
-        >
-          {navbarLinks.map((item, index) => {
-            return (
-              <li className="navbar__item" key={index}>
-                <Link
-                  className="navbar__link"
-                  activeClass="active"
-                  to={`${item.url}`}
-                  spy={true}
-                  smooth={true}
-                  offset={-10}
-                  duration={1200}
-                  onClick={closeMenu}
-                >
-                  {" "}
-                  {item.title}{" "}
-                </Link>
-                {/* <a className="navbar__link" href={item.url}>
-                {item.title}
-              </a> */}
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </div>
-  );
-};
+      </div>
 
-export default Header;
+<video autoPlay loop muted id='video'>
+        <source src={smartphonecheckvideo} type='video/mp4'/>
+      </video>
+      
+      
+      
+      </div>
+  )
+}
+
+export default Header
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,100 @@
-import React from 'react'
-import './Navbar.css'
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { FiMenu, FiX } from "react-icons/fi";
+import { BsTwitter } from "react-icons/bs";
+import { FaDiscord } from "react-icons/fa";
+import { NavLink, Link } from "react-router-dom";
+import useFetch from "../../constants/useFetch";
+
 
 const Navbar = () => {
 
-  const API_URL_HEADERNAV = "https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?access_token=2ez5kqa9TZOM-3B_THRj0kQkOUwTyCCf24WldszChbc&select=fields&content_type=headernav"
-    
+  const [menuClicked, setMenuClicked] = useState(false);
 
-    const navbarliste = (list) => {
-        list.map((item) => {
-            <a href='' key={uuidv4()}>  </a>
-        })
-    }
+  const toggleMenuClick = () => {
+    setMenuClicked(!menuClicked);
+  };
+
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    const closeMenu = () => setClick(false)
+
+    const API_URL_HEADER =
+"https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?select=fields&content_type=headernav";
+const API_URL_NAVBAR =
+"https://cdn.contentful.com/spaces/xwxqqpx0he6y/entries/?select=fields&content_type=navbar";
+
+const { data, loading, error } = useFetch(API_URL_HEADER);
+const {data: navbar, loading: navbarloading, error: naverror} = useFetch(API_URL_NAVBAR)
+
+const navbarLinks = [ 
+  {url: "home", title: "Home"},
+  {url: "random", title: "Random"},
+  {url: "team", title: "Team"},
+    ]
+
+let headerurl = null;
+let navbaritem = null;
+let navbaritems = null;
+
+if (data && navbar) {
+    console.log(navbar)
+    console.log(data);
+    const headertitle = data["items"][0]["fields"]["headertitle"];
+    headerurl = data["includes"]["Asset"][0]["fields"]["file"]["url"];
+
+    navbaritems = navbar.includes.Entry.map((item, index) => {
+
+     return(
+      <li className="navbar__item" key={index}>
+              {/* <NavLink className="navbar__link" activeClass="active" to={`${item.fields.url}`} spy={true} smooth={true} offset={50} duration= {1200} onClick={closeMenu}  > {item.fields.title}  </NavLink> */}
+              {/* <Link className="navbar__link" activeClass="active" to={`${item.fields.url}`} onClick={closeMenu}  > {item.fields.title}  </Link> */}
+              <a className="navbar__link" href={item.fields.url}>
+                {item.fields.title}
+              </a>
+            </li>
+     )
+    })
+  
+  }
+
+
+
+
   return (
-    <div className='navbar'>
-      
-      
-      Navbar</div>
-  )
-}
+    <div className="navbar__div">
+    <nav className="navbar">
+      <span className="navbar__logo"><img height={150} src={headerurl}/></span>
+      <div className="navbar__socials">
+        <a className="navbar__socials_a" href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer">
+         <BsTwitter className="navbar__socials_twitter" href="https://twitter.com" title="Twitter" target="_blank" rel="noopener noreferrer"/>
+        </a>
+        <a className="navbar__socials_a_discord" href="http://www.discord.gg"  title="Discord" target="_blank" rel="noopener noreferrer">
+         <FaDiscord className="navbar__socials_discord"  title="Discord" target="_blank" rel="noopener noreferrer"/>
+        </a>
+        </div>
+        
+      {menuClicked ? (
+        <FiX size={25} className={"navbar__menu"} onClick={toggleMenuClick} />
+      ) : (
+        <FiMenu
+          size={25}
+          className={"navbar__menu"}
+          onClick={toggleMenuClick}
+        />
+      )}
+      <ul
+        className={
+          menuClicked ? "navbar__list navbar__list--active" : "navbar__list"
+        }
+      >
+        {navbaritems}
+    
+      </ul>
+    </nav>
+    </div>
+  );    
+};
 
-export default Navbar
+export default Navbar;
